37bb042008ce31fb322b14bc7b6fc6b53162fcee
cynthiawupore <cynthiawuqian@outlook.com>
2016-03-31 12:00:37 +0800

    0903

---
+++ js/seven.js
@@ -0,0 +1,31 @@
+$(function () {
+    $('#container7').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Stacked bar chart'
+        },
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total fruit consumption'
+            }
+        },
+        legend: {
+            reversed: true
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
+        series: [{
+            name: 'John',
+            data: [3, 4, 4, 2, 5]
+        }]
+    });
+});
